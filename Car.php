51304a42db5ced690a41ae32675191462a9790d9
Alex Fallenstedt and Michael Lor <student@epicodus.com>
2016-02-08 09:11:37 -0800

    add worthBuying function

--- Car.php
+++ Car.php
@@ -4,15 +4,20 @@ class Car
     public $make_model;
     public $price;
     public $miles;
+
+    function worthBuying($max_price)
+    {
+        return $this->price <= ($max_price + 100);
+    }//this function compares the price of each car to the user's input (max_price)
+    //retun true or false
 }
 
 $porsche = new Car();
-$porshe->make_model = "2014 Porshe 911";
+$porsche->make_model = "2014 Porshe 911";
 $porsche->price = 110000;
 $porsche->miles = 7864;
 
-
-$ford = new Car();
+$ford = new Car();//a car object
 $ford->make_model = "2011 Ford F450";//properties
 $ford->price = 55000;
 $ford->miles = 14241;
@@ -31,10 +36,11 @@ $cars = array($porsche, $ford, $lexus, $mercedes);
 
 $cars_matching_array = array();
 foreach ($cars as $car) {
-    if ($car->price < $_GET["price"]) {
+    if ($car->worthBuying($_GET["price"])) {
         array_push($cars_matching_array, $car);
   }
-}
+}//for each car in our cars array, we will run the "worthBuying" function on it.
+//if the function returns true, then it will push that car into the cars_matching_array.
 ?>
 
 <!DOCTYPE html>
