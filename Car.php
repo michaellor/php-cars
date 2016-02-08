4d28b178cb9dea494afb41d6da8d8e68a1858f61
Alex Fallenstedt and Michael Lor <michael.vang.1985@gmail.com>
2016-02-08 11:05:23 -0800

    make setter and getter for price

--- Car.php
+++ Car.php
@@ -2,7 +2,7 @@
 class Car
 {
     private $make_model;
-    public $price;
+    private $price;
     public $miles;
 
     function setMakeModel($new_model)
@@ -15,6 +15,15 @@ class Car
       return $this->make_model;
     }
 
+    function setPrice($new_price)
+    {
+        $this->price = (float) $new_price;
+    }
+
+    function getPrice()
+    {
+        return $this->price;
+    }
 
     function __construct($make_model, $price, $miles)
     {
@@ -25,7 +34,10 @@ class Car
 
     function worthBuying($max_price)
     {
-        return $this->price <= ($max_price + 100);
+        if ($this->price <= $max_price) {
+          return true;
+        }
+
     }//this function compares the price of each car to the user's input (max_price)
     //retun true or false
 }
@@ -34,7 +46,16 @@ $porsche = new Car("2014 Porshe 911", 110000, 7864);
 $subaru = new Car("2002 Subaru Outback", 3800, 128000);
 $toyota = new Car("1994 Toyota Tacoma", 5000, 195000);
 $ford = new Car("1997 Ford Wrangler", 1000, 300000);
+
+$porsche->setMakeModel("2014 Porsche 911");
 $subaru->setMakeModel("2002 Subaru Forester");
+$toyota->setMakeModel("1994 Toyota Tacoma");
+$ford->setMakeModel("1997 Ford Wrangler");
+
+$porsche->setPrice(110000.07);
+$subaru->setPrice(3400.34);
+$toyota->setPrice(5000.99);
+$ford->setPrice(1001.19);
 
 
 $cars = array($porsche, $subaru, $toyota, $ford);
@@ -59,9 +80,10 @@ foreach ($cars as $car) {
     <?php
       foreach ($cars_matching_array as $car) {
           $make_model = $car->getMakeModel();
-          echo "<li> $make_model </li>";
+          $price = $car->getPrice();
+          echo "<li>$make_model</li>";
           echo "<ul>";
-              echo "<li> $car->price </li>";
+              echo "<li> $price </li>";
               echo "<li> $car->miles </li>";
           echo "</ul>";
       }
